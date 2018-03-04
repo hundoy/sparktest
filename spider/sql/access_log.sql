92c9ab11a7b34ff6a065ff3da2275db5a4308fae
hundoy <zoharwolf@qq.com>
2018-03-04 23:22:35 +0800

    add access log sql

---
+++ spider/sql/access_log.sql
@@ -0,0 +1,53 @@
+-- 每天访问量
+select count(1),to_char(access_time,'YYYY-MM-DD') as ad from access_log group by ad order by count(1) desc;
+
+-- 省PV
+select count(1),province  from access_log group by province order by count(1) desc;
+-- 省UV
+select count(distinct ip),province from access_log group by province order by count(distinct ip) desc;
+
+-- 省市PV
+select count(1),province,city from access_log group by province,city order by count(1) desc;
+-- 省市UV
+select count(distinct ip),province,city from access_log group by province,city order by count(distinct ip) desc;
+
+-- 省市PV  国外
+select count(1),country from access_log 
+where country<>'中国'
+group by country
+order by count(1) desc;
+-- 省市UV 国外
+select count(distinct ip),country from access_log 
+where country<>'中国'
+group by country order by count(distinct ip) desc;
+
+
+-- 用户信息访问量
+select count(1), al.uid,u.nick_name
+from access_log al
+left join kuser u on al.uid=u.uid
+where page='display_detail'
+group by al.uid,u.nick_name
+order by count(1) desc;
+-- UV
+select count(distinct al.ip), al.uid,u.nick_name
+from access_log al
+left join kuser u on al.uid=u.uid
+where page='display_detail'
+group by al.uid,u.nick_name
+order by count(distinct al.ip) desc;
+
+-- nodata 
+select count(1), al.uid,u.nick_name
+from access_log al
+left join kuser u on al.uid=u.uid
+where page='nodata'
+group by al.uid,u.nick_name
+order by count(1) desc;
+-- UV
+select count(distinct al.ip), al.uid,u.nick_name
+from access_log al
+left join kuser u on al.uid=u.uid
+where page='nodata'
+group by al.uid,u.nick_name
+order by count(distinct al.ip) desc;
